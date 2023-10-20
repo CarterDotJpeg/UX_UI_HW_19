41568cb9a753b0c478a9b046ad3dbb921f8e2c73
CarterDotJpeg <carterandersonbell@gmail.com>
2023-10-20 00:37:14 -0700

    Submia\ssion

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Here is a JavaScript file used to code the vault interaction
+
+$(".preloader img").on("click", function() {
+    console.log("This is doing what it's supposed to do.");
+    $('.preloader').addClass('moveLeft');
+    $('.preloader img').addClass('spin');
+});
+//setTimeout(fade, 3000);
